7100f3a78151171e1a86980cf9f34dbf7d20b848
ahama <mohammad_91225@yahoo.com>
2019-12-27 19:16:19 -0800

    These changes are made in ver2.0

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 
 def main(message):
 
-    // This comment is when I started using version control in pycharm.
-
-// This was the conflict, pretty neat ey!
-<<< HEAD
-    // This comment however is from pi dir
-    // Let's see what conflict  arises
-=======
-    // This is to print a message
-    // This file is from pi2 dir on my local machine
->>> refs/remotes/origin/master
+    // This comment is from ver2.0
     print(message)
 
+def second():
+    print("a second function")
 
 main("Hello beautiful")
